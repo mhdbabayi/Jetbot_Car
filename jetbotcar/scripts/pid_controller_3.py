a557d1de4c2b96b75927da7cd4e91a9be43e3053
lzkevinnn <1171298332@qq.com>
2021-04-01 23:06:53 +0100

    pid first operating(need calibration), next step: create start and stop button

--- jetbotcar/scripts/pid_controller_3.py
+++ jetbotcar/scripts/pid_controller_3.py
@@ -23,11 +23,10 @@ kp = 1
 kd = 0.5
 ki = 0
 # lateralError = 0.0
-
+# cumError = 0.0
 Setpoint = 0.0 # follow the middle of the line is set as 0.0
 
 Ts = 1
-cumError = 0.0
 lastError = 0.0
 
 # INITIALISE THE THROTTLE AND STEERING CMDs
@@ -38,16 +37,19 @@ steeringGain = 0.0
 
 def pidCallback(msg):
 
-    global startTime
+    # global startTime
+    global previousTime
     global currentTime
     global Setpoint
     global Ts
-    global cumError
+    # global cumError
     global lastError
     # global lateralError
 
     global kp, ki, kd
     global throttleCmd, steeringCmd
+
+    cumError = 0.0
 
     publish = bool(True)
 
@@ -60,14 +62,14 @@ def pidCallback(msg):
     currentTime = rospy.get_time()
     rospy.loginfo("Current time: %.2f", currentTime)
 
-    elapsedTime = currentTime - startTime
+    elapsedTime = currentTime - previousTime
     rospy.loginfo("Elapsed time: %.2f", elapsedTime)
 
     # Compute all the working error variables, careful with the sign convension
     error = lateralErrorCmd #lateral error is the error input from image processing
     rospy.loginfo("lateral error: %.2f", error)
 
-    # cumError = cumError + error * elapsedTime
+    cumError = cumError + error * elapsedTime
     # cumError = integrate.quad(error, 0, 0.1)
     rospy.loginfo("Cumulative Error: %.2f", cumError)
 
@@ -84,47 +86,42 @@ def pidCallback(msg):
     lastError = error
     previousTime = currentTime
 
+
+
     # output conditions need to be modified
     ###################################
-    if output > 0 and output < 5:
-        throttleCmd = 0.5
+    if output > 0 and output < 1:
+        throttleCmd = 0.0
         steeringCmd = output # probably 
         
-    elif output > 5:
-        throttleCmd = 0.5
+    elif output > 1:
+        throttleCmd = 0.0
         steeringCmd = 0.8 # saturation the max steering, condition need further calibration
 
-    elif output > -5 and output < 0:
-        throttleCmd = 0.5
+    elif output > -1 and output < 0:
+        throttleCmd = 0.0
         steeringCmd = output
 
-    elif output < -5:
-        throttleCmd = 0.5
+    elif output < -1:
+        throttleCmd = 0.0
         steeringCmd = -0.8 # saturation the max steering, condition need further calibration
 
     elif output == 0:
-        throttleCmd = 0.5
+        throttleCmd = 0.0
         steeringCmd = output
     else:
         publish = bool(False)
     ###################################
 
 
-    # if publish:
-    #     self.publishJetracer(throttle, steering)
-
-
 def main():
     global throttleCmd, steeringCmd
-    global startTime
     global currentTime
     global previousTime
+
     # initialise pid_controller node
     rospy.init_node("pid_controller", anonymous=True)
-
- 
-    startTime = rospy.get_time() # startTime is constant now, stamped straight after init_node
-    rospy.loginfo("startTime: %.2f", startTime)
+    previousTime = rospy.get_time() # startTime is constant now, stamped straight after init_node
 
     # subscribes to the lateral_error topic
     rospy.Subscriber("/lateral_error", Float64, pidCallback)
@@ -145,7 +142,7 @@ def main():
         drive_msg.throttle = throttleCmd
         drive_msg.steering = steeringCmd
 
-        # drive_pub.publish(drive_msg) # from now don't publish drive_msg to LLC
+        drive_pub.publish(drive_msg) # from now don't publish drive_msg to LLC
 
         # print("Publishing control commands for throttle and steering")
         rate.sleep()
